1293d657d9781fb109d21fb0adb032daf87870f7
Peter Herbel <herbel.peter@windowslive.com>
2016-11-08 13:25:48 +0100

    Update to NetStandard

---
+++ src/DnsRip/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DnsRip")]
+[assembly: AssemblyDescription("A .Net component for easily doing DNS lookups")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ed Caicedo")]
+[assembly: AssemblyProduct("DnsRip")]
+[assembly: AssemblyCopyright("Copyright © Ed Caicedo 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4b3a5a1c-122d-46f6-83a5-567f13e42fad")]
---
+++ src/DnsRip/Utilites/SocketHelper.cs
@@ -0,0 +1,62 @@
+﻿using DnsRip.Exceptions;
+using DnsRip.Models;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace DnsRip.Utilites
+{
+    public class SocketHelper : IDisposable
+    {
+        public SocketHelper(DnsRequest request, string server, TimeSpan timeout)
+        {
+            try
+            {
+                IPAddress ip;
+                IPAddress.TryParse(server, out ip);
+
+                if (ip == null)
+                {
+                    IPHostEntry hostEntry = Dns.GetHostEntryAsync(server).GetAwaiter().GetResult();
+                    IPAddress.TryParse(hostEntry.AddressList[0].ToString(), out ip);
+
+                }
+
+                _server = new IPEndPoint(ip, 53);
+            }
+            catch
+            {
+                throw new InvalidNameserverException(server);
+            }
+
+            _request = request;
+            _timeout = timeout;
+        }
+
+        private Socket _socket;
+        private readonly IPEndPoint _server;
+        private readonly DnsRequest _request;
+        private readonly TimeSpan _timeout;
+
+        public byte[] Send()
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            _socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, (int)_timeout.TotalMilliseconds);
+            _socket.SendTo(_request.Data, _server);
+
+            var buffer = new byte[512];
+            var received = _socket.Receive(buffer);
+            var data = new byte[received];
+
+            Array.Copy(buffer, data, received);
+
+            return data;
+        }
+
+        public void Dispose()
+        {
+            _socket.Dispose();
+        }
+    }
+}
